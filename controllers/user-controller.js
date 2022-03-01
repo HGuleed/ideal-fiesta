e56d28f93e426f386b89b6f89f7d87b06f559efe
Hodan <you@example.com>
2022-03-01 18:55:02 -0500

    finished routes, working on controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,7 @@
+const User = require("../models");
+
+const userController = {
+    getAllUser(req,res){
+        User.find({}).
+    }
+}
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost/social-media-startup", {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set("debug", true);
+
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
